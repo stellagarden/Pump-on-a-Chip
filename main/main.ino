bed085b3b245111c76f47aa13d2fa0b94d3558b7
Hyemin Stella Lee <byj4565@kaist.ac.kr>
2023-04-23 11:56:44 -0400

    Update main.ino to include uLCD

--- main/main.ino
+++ main/main.ino
@@ -2,10 +2,12 @@
 // FileName     : main.ino
 // Author       : Hyemin Stella Lee
 // Created      : 04/19/2023
-// Description  : Runs overall pump-on-ac-chip system
+// Description  : Runs overall pump-on-a-chip system
 // -----------------------------------------------------------
 
 #include <Wire.h>
+#include "Goldelox_Serial_4DLib.h"
+#include "Goldelox_Const4D.h"
 
 // Gauge
 #define GAUGE_ADDR 56 // 0x38
@@ -21,6 +23,19 @@ void solPinch() {
   solPinchState = !solPinchState;
 }
 
+// uLCD
+#define DisplaySerial Serial1
+#define LCD_Reset 8
+Goldelox_Serial_4DLib Display(&DisplaySerial);
+
+void LCD_print(float a, float b) {
+  Display.txt_MoveCursor(2,0) ;
+  Display.print(a);
+  Display.txt_MoveCursor(6,0) ;
+  Display.print(b);
+}
+
+
 void setup() {
   // Gauge
   Serial.begin(9600);
@@ -34,6 +49,21 @@ void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(BUTTON_PIN, INPUT);
   attachInterrupt(digitalPinToInterrupt(BUTTON_PIN), solPinch, RISING);
+
+  // uLCD
+  pinMode(LCD_Reset, OUTPUT);
+  digitalWrite(LCD_Reset, 0);
+  delay(100);
+  digitalWrite(LCD_Reset, 1);
+  delay(3000);
+  Display.TimeLimit4D = 3000 ; // 3 second timeout on all commands
+  DisplaySerial.begin(9600);
+  Display.txt_FGcolour(WHITE);
+  Display.gfx_Cls() ;
+  Display.txt_Height(2) ;
+  Display.txt_Width(2) ;
+  Display.putstr("Pump (mbar)") ;
+  Display.putstr("\n\n\n\nChip (mbar)");
 }
 
 void loop() {
@@ -44,14 +74,7 @@ void loop() {
     rawdata = rawdata << 8;
     rawdata |= Wire.read();
     pressure = 0.316456*rawdata-1580.74;  // Convert raw data to actual pressure
-    Serial.print(pressure);
-    Serial.print(" mbar | P_at + ");
-    Serial.print(pressure-1013.25);
-    Serial.println(" mbar");
-    Serial1.print(pressure);
-    Serial1.print(" mbar | P_at + ");
-    Serial1.print(pressure-1013.25);
-    Serial1.println(" mbar");
+    LCD_print(pressure, 2000.0-pressure);
   }  
 
   // Solenoid Pinch

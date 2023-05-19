12505f12b0f5443c6b66b531c7fc30d5dc0d377e
Hyemin Stella Lee <byj4565@kaist.ac.kr>
2023-05-19 15:55:45 -0400

    Create I2C_flowrate_example.ino

---
+++ I2C_flowrate_example/I2C_flowrate_example.ino
@@ -0,0 +1,41 @@
+// -----------------------------------------------------------
+// FileName     : I2C_flowrate_example.ino
+// Author       : Hyemin Stella Lee
+// Created      : 05/18/2023
+// Description  : Read flowrate from the sensor and print it as ml/min
+//                via serial monitor in PC
+// -----------------------------------------------------------
+
+#include <Wire.h>
+
+#define FLOWRATE_ADDR 8 // 0x38
+uint16_t rawdata = 0;
+float flowrate = 0;
+
+void setup(){
+  Serial.begin(9600);
+  Serial1.begin(9600);
+  Wire.begin();
+  Wire.beginTransmission(FLOWRATE_ADDR);
+  Wire.write(0x3608); // Start continuous reading
+  Wire.endTransmission();
+}
+
+void loop(){
+  Wire.requestFrom(FLOWRATE_ADDR, 2);  // requests 2 bytes from the specified address
+  if (Wire.available() > 0){
+    rawdata = Wire.read();
+    rawdata = rawdata << 8;
+    pressure = 0.316456*rawdata-1590.23;  // Convert raw data to actual pressure
+    rawdata |= Wire.read();
+    Serial.print(pressure);
+    Serial.print(" mbar | P_at + ");
+    Serial.print(pressure-1013.25);
+    Serial.println(" mbar");
+    Serial1.print(pressure);
+    Serial1.print(" mbar | P_at + ");
+    Serial1.print(pressure-1013.25);
+    Serial1.println(" mbar");
+  }  
+  delay(100);
+}

e3fcd1b196f7c246c946b8147df9829a43c8aebf
Hyemin Stella Lee <byj4565@kaist.ac.kr>
2023-06-13 22:33:48 -0400

    Added radiobuttons and start button

--- raspberrypi_GUI.py
+++ raspberrypi_GUI.py
@@ -3,87 +3,145 @@ import time
 import tkinter as tk
 
 # Define buttons
-def quit():
-    global tkTop
-    # ser.write(bytes('L', 'UTF-8'))
-    tkTop.destroy()
+def start():
+    global inputType
+    global status
+    global input_pressure
+    global input_flowrate
+    # Change status
+    status.set("Pressurizing")
+    display_status.config(fg="black",bg="white")
 
-def set_button1_state(): 
-        varLabel.set("LED ON ")
-        # ser.write(bytes('H', 'UTF-8'))
-        varLabel2.set(b)
-        print(b)
+    # Send inputType and input to Arduino
+    if (inputType.get()):
+        # Selected Flowrate
+        # ser.write(bytes(inputType.get(),'UTF-8'))
+        # ser.write(bytes(input_flowrate.get(),'UTF-8'))
+        print("Send: "+str(inputType.get()))
+        print("Send: "+input_flowrate.get())
+    else:
+        # Selected Pressure
+        # ser.write(bytes(inputType.get(),'UTF-8'))
+        # ser.write(bytes(input_pressure.get(),'UTF-8'))
+        print("Send: "+str(inputType.get()))
+        print("Send: "+input_pressure.get())
 
-def set_button2_state():
-        varLabel.set("LED OFF")
-        # ser.write(bytes('L', 'UTF-8'))
-
-# Connect serial with Arduino
+# # Connect serial with Arduino
 # ser = serial.Serial('com7', 9600)
 # print("Reset Arduino")
 # time.sleep(3)
-# ser.write(bytes('L', 'UTF-8'))
 
 # Make GUI
 tkTop = tk.Tk()
-tkTop.geometry('400x400')
 tkTop.title("Pump-on-a-Chip v.1.1")
 
+# Global variables
+inputType = tk.IntVar()
 status = tk.StringVar()
+status.set("Ready")
+
+# Status
 display_status = tk.Label(tkTop,
     textvariable=status,
     font=("Arial", 20),
-    borderwidth=1,
+    padx=30,
+    pady=5,
     relief="solid",
     fg="black",
     bg="green",
     width=20,
     height=1
 )
-status.set("Ready")
-display_status.pack(pady=10)
+display_status.grid(row=0,column=0,columnspan=6)
 
-varLabel = tk.StringVar()
-tkLabel = tk.Label(textvariable=varLabel, )
-tkLabel.pack()
-
-varLabel2 = tk.IntVar()
-tkLabel2 = tk.Label(textvariable=varLabel2, )
-tkLabel2.pack()
-
-button1 = tk.IntVar()
-button1state = tk.Button(tkTop,
-    text="ON",
-    command=set_button1_state,
-    height = 4,
-    fg = "black",
-    width = 8,
-    bd = 5,
-    activebackground='green'
+# Initial input - Pressure
+tk.Radiobutton(tkTop,
+    text="Pressure: ",
+    font=("Arial", 10),
+    variable=inputType,
+    value=0
+).grid(row=1,column=0)
+input_pressure = tk.Entry(tkTop,
+    width=15,
+    font=("Arial", 10),
+    borderwidth=5
 )
-button1state.pack(side='top', ipadx=10, padx=10, pady=15)
+input_pressure.grid(row=1,column=1)
+tk.Label(tkTop,
+    text="mbar",
+    font=("Arial", 10)
+).grid(row=1,column=2)
 
-button2 = tk.IntVar()
-button2state = tk.Button(tkTop,
-    text="OFF",
-    command=set_button2_state,
-    height = 4,
-    fg = "black",
-    width = 8,
-    bd = 5
+# Initial input - flowrate
+tk.Radiobutton(tkTop,
+    text="Flowrate: ",
+    font=("Arial", 10),
+    # padx=5,
+    # pady=5,
+    variable=inputType,
+    value=1
+).grid(row=2,column=0)
+input_flowrate = tk.Entry(tkTop,
+    width=15,
+    font=("Arial", 10),
+    borderwidth=5
 )
-button2state.pack(side='top', ipadx=10, padx=10, pady=15)
+input_flowrate.grid(row=2,column=1)
+tk.Label(tkTop,
+    text="ml/min",
+    font=("Arial", 10)
+).grid(row=2,column=2)
 
-tkButtonQuit = tk.Button(
-    tkTop,
-    text="Quit",
-    command=quit,
-    height = 4,
-    fg = "black",
-    width = 8,
-    bg = 'yellow',
-    bd = 5
-)
-tkButtonQuit.pack(side='top', ipadx=10, padx=10, pady=15)
+# Start button
+tk.Button(tkTop,
+    text="START",
+    command=start,
+    height=4,
+    width=15
+).grid(row=1,column=3,rowspan=2)
+
+
+# varLabel = tk.StringVar()
+# tkLabel = tk.Label(textvariable=varLabel, )
+# tkLabel.pack()
+
+# varLabel2 = tk.IntVar()
+# tkLabel2 = tk.Label(textvariable=varLabel2, )
+# tkLabel2.pack()
+
+# button1 = tk.IntVar()
+# button1state = tk.Button(tkTop,
+#     text="ON",
+#     command=set_button1_state,
+#     height = 4,
+#     fg = "black",
+#     width = 8,
+#     bd = 5,
+#     activebackground='green'
+# )
+# button1state.pack(side='top', ipadx=10, padx=10, pady=15)
+
+# button2 = tk.IntVar()
+# button2state = tk.Button(tkTop,
+#     text="OFF",
+#     command=set_button2_state,
+#     height = 4,
+#     fg = "black",
+#     width = 8,
+#     bd = 5
+# )
+# button2state.pack(side='top', ipadx=10, padx=10, pady=15)
+
+# tkButtonQuit = tk.Button(
+#     tkTop,
+#     text="Quit",
+#     command=quit,
+#     height = 4,
+#     fg = "black",
+#     width = 8,
+#     bg = 'yellow',
+#     bd = 5
+# )
+# tkButtonQuit.pack(side='top', ipadx=10, padx=10, pady=15)
 
 tk.mainloop()

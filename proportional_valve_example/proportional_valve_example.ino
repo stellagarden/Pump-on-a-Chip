d826948825b3b97ece3c8cd4ae0744508fb18b8d
Hyemin Stella Lee <byj4565@kaist.ac.kr>
2023-07-03 10:41:53 -0400

    Change propor valve pin

--- proportional_valve_example/proportional_valve_example.ino
+++ proportional_valve_example/proportional_valve_example.ino
@@ -7,24 +7,26 @@
 
 #include <Wire.h>
 
-#define PROPOR_PIN A0
+#define PROPOR_PIN 1
 
 int open = 0;
 
 void setup() {
   Serial.begin(9600);
-  pinMode(LED_BUILTIN, OUTPUT);
 
   pinMode(PROPOR_PIN, OUTPUT);
   analogWriteResolution(10);
 }
 
 void loop() {
-  Serial.println(open);
-  open = 1023;
-  analogWrite(PROPOR_PIN, open);
-  delay(1500);
-  open = 0;
-  analogWrite(PROPOR_PIN, open);
-  delay(1500);
+  analogWrite(PROPOR_PIN, 1023);
+  delay(3000);
+  analogWrite(PROPOR_PIN, 750);
+  delay(3000);
+  analogWrite(PROPOR_PIN, 512);
+  delay(3000);
+  analogWrite(PROPOR_PIN, 256);
+  delay(3000);
+  analogWrite(PROPOR_PIN, 0);
+  delay(3000);
 }

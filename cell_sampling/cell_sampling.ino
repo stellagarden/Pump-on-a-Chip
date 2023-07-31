34c43ac84b2fd53d86b2d84ae7d5f03587f9b8bb
Hyemin Stella Lee <byj4565@kaist.ac.kr>
2023-07-30 23:40:56 -0400

    Final

--- cell_sampling/cell_sampling.ino
+++ cell_sampling/cell_sampling.ino
@@ -9,7 +9,7 @@
 #define PINCH1_PIN 1
 #define PINCH2_PIN 2
 #define PINCH3_PIN 3
-const unsigned long cellLoadTimer = 10000;
+const unsigned long cellLoadTimer = 15000;
 const unsigned long discardTimer = 5000;
 
 #define accumulatedMillis millis() - timerMillis
